017e5db6c412535da77db131736c67a1146cd03f
Grace-Foreman <135276519+Grace-Foreman@users.noreply.github.com>
2024-05-08 20:24:56 -0400

    drive system

--- src/main/java/frc/robot/subsystems/DriveSubsystems.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystems.java
@@ -8,6 +8,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
+
+
+// declare all of our motors. use the constants for the port
+
+
+
   /** Creates a new ExampleSubsystem. */
   public DriveSubsystem() {}
 
