8d02c5d10ad4b6a733bfa365fa2a4abb2453e293
Grace-Foreman <135276519+Grace-Foreman@users.noreply.github.com>
2024-05-29 20:31:21 -0400

    intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,84 +8,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DriveSubsystem extends SubsystemBase {
+public class intakeSubsystem extends SubsystemBase {
 
 
 // declare all of our motors. use the constants for the port
-
-  CANSparkMax frontLeftDriveMotor = new CANSparkMax(kFrontLeftDrivePort, MotorType.kBrushless);
-  CANSparkMax backLeftDriveMotor = new CANSparkMax(kBackLeftDrivePort, MotorType.kBrushless);
-  CANSparkMax frontRightDriveMotor = new CANSparkMax(kFrontRightDrivePort, MotorType.kBrushless);
-  CANSparkMax backRightDriveMotor = new CANSparkMax(kBackRightDrivePort, MotorType.kBrushless);
-
-   //declare Encorders
-  RelativeEncoder frontLeftEncoder;
-  RelativeEncoder frontRightEncoder;
-  RelativeEncoder backLeftEncoder;
-  RelativeEncoder backRightEncoder;
-  RelativeEncoder leftShootEncoder;
-  RelativeEncoder rightShootEncoder;
-
-  // delcare gyro
-  AHRS gyro = new AHRS(SPI.Port.kMXP);
-
-  DifferentialDrive m_robotDrive;
+ CANSparkMax leftIntake = new CANSparkMax(7, MotorType.kBrushed);
+ CANSparkMax rightIntake = new CANSparkMax(8, MotorType.kBrushed);
 
 
+
+ 
   /** Creates a new ExampleSubsystem. */
-  public DriveSubsystem() {
+  public intakeSubsystem() {
      
-    // initialize motors
-    frontLeftDriveMotor.restoreFactoryDefaults();
-    frontRightDriveMotor.restoreFactoryDefaults();
-    backRightDriveMotor.restoreFactoryDefaults();
-    backLeftDriveMotor.restoreFactoryDefaults();
-    
-    // set motor idle mode to brake
-    frontLeftDriveMotor.setIdleMode(IdleMode.kBrake);
-    frontRightDriveMotor.setIdleMode(IdleMode.kBrake);
-    backRightDriveMotor.setIdleMode(IdleMode.kBrake);
-    backLeftDriveMotor.setIdleMode(IdleMode.kBrake);
+    // set motor inversion 
+  rightIntake.setInverted(false);
+  leftIntake.setInverted(true);
    
-    // set ramp rate to 1 s
-    frontLeftDriveMotor.setClosedLoopRampRate(1);
-    frontRightDriveMotor.setClosedLoopRampRate(1);
-    backRightDriveMotor.setClosedLoopRampRate(1);
-    backLeftDriveMotor.setClosedLoopRampRate(1);
-
-    // initialize encoders
-    backLeftEncoder = backLeftDriveMotor.getEncoder(Type.kHallSensor, 42);
-    backRightEncoder = backRightDriveMotor.getEncoder(Type.kHallSensor, 42);
-    frontLeftEncoder = frontLeftDriveMotor.getEncoder(Type.kHallSensor, 42);
-    frontRightEncoder = frontRightDriveMotor.getEncoder(Type.kHallSensor, 42);
-
-    // set for the wheel motors as we want to know the positions
-    //  PI * WheelDiameter / GearRatio --WheelDiameter is in inches
-    frontLeftEncoder.setPositionConversionFactor(Math.PI*6/8.45);
-    frontRightEncoder.setPositionConversionFactor(Math.PI*6/8.45);
-    backLeftEncoder.setPositionConversionFactor(Math.PI*6/8.45);
-    backRightEncoder.setPositionConversionFactor(Math.PI*6/8.45);
-    
-      // set leader/followers - this connects the front and back motors to drive together
-    backLeftDriveMotor.follow(frontLeftDriveMotor);
-    backRightDriveMotor.follow(frontRightDriveMotor);
-    //.
-
-    // set motor inversion (may not have to do this - test without it later)
-    frontLeftDriveMotor.setInverted(true);
-    backLeftDriveMotor.setInverted(true);
-    frontRightDriveMotor.setInverted(false);
-    backRightDriveMotor.setInverted(false); // have to do seperate for each motor
-
-      // burn settings into memory
-    frontLeftDriveMotor.burnFlash();
-    frontRightDriveMotor.burnFlash();
-    backRightDriveMotor.burnFlash();
-    backLeftDriveMotor.burnFlash();
-        
-    m_robotDrive = new DifferentialDrive(frontLeftDriveMotor,frontRightDriveMotor); //all motors connected
-
-
   }
 
   /**
@@ -107,28 +46,21 @@ public class DriveSubsystem extends SubsystemBase {
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-  public void drive(double forward, double rotation){
-    m_robotDrive.arcadeDrive(forward, rotation);
-  }
+ 
+  
 
-  public void resetEncoders(){
-    backLeftEncoder.reset();
-     backRightEncoder.reset();
-     frontLeftEncoder.reset();
-     frontRightEncoder.reset();
-  }
+  private void intake(double speed) {
+    rightIntake.set(speed);
+    leftIntake.set(speed);
+  }   // end intake
 
-  public void stopDriving() {
-    m_robotDrive.arcadeDrive(0,0);
-  }
+    
+  private void intakeStop(double speed) {
+    rightIntake.set(0);
+    leftIntake.set(0);
+  }   // end intake
 
-  public void resetGyro(){
-    gyro.reset();
-  }
+  
 
   @Override
   public void periodic() {
